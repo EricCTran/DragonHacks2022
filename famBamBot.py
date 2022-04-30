5eeee64ec92ff3a39b99dfba30d461c84e122c9e
Lisa <lisa.chaing.tran.116@gmail.com>
2022-04-30 14:57:33 -0400

    Commands

--- famBamBot.py
+++ famBamBot.py
@@ -1,4 +1,5 @@
 import hikari
+import lightbulb
 
 token = open('famBamBotToken.txt', 'r')
 
@@ -6,8 +7,9 @@ tokenStr = token.read()
 
 token.close()
 
-bot = hikari.GatewayBot(token=tokenStr)
+bot = lightbulb.BotApp(token=tokenStr, default_enabled_guilds=(969993622697177168))
 
+#Part 2
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
     print(event.content)
@@ -15,5 +17,34 @@ async def print_message(event):
 @bot.listen(hikari.StartedEvent)
 async def bot_started(event):
     print('Bot has started')
+
+#Part 3
+@bot.command
+@lightbulb.command('ping','says pong')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+@bot.command
+@lightbulb.command('group','This is a group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    pass
+
+@my_group.child
+@lightbulb.command('subcommand','This is a subcommand')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand!')
+
+@bot.command
+@lightbulb.option('num1','The first number', type=int)
+@lightbulb.option('num2','The second number', type=int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
+
+
 
 bot.run()
