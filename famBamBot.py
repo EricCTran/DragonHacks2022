679f66f534030ac19625c7d98deb261d87a2c697
Lisa <lisa.chaing.tran.116@gmail.com>
2022-04-30 14:14:47 -0400

    pt 2

--- famBamBot.py
+++ famBamBot.py
@@ -7,4 +7,13 @@ tokenStr = token.read()
 token.close()
 
 bot = hikari.GatewayBot(token=tokenStr)
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.content)
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print('Bot has started')
+
 bot.run()
